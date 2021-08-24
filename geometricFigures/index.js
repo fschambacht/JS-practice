4b23b575ce88f87792b9c584a4eff317dbfdc6a9
fschambacht <aschambacht@gmail.com>
2021-08-24 16:49:15 -0500

    create functions to get area and perimeter of geometric figures

--- geometricFigures/index.js
+++ geometricFigures/index.js
@@ -1,36 +1,34 @@
 // square
-const sideSquare =5;
-const perimeterSquare = sideSquare * 4;
-const areaSquare = sideSquare * sideSquare;
+function perimeterSquare(sideSquare){
+    let perimeter = sideSquare * 4;
+    return console.log("El perimetro del cuadrado es: " + perimeter + " cm");
+}
 
-console.group("Square's");
-console.log("Los lados del cuadrado miden: " + sideSquare + " cm");
-console.log("El perimetro del cuadrado es: " + perimeterSquare + " cm");
-console.log("El area del cuadrado es: " + areaSquare + " cm²");
-console.groupEnd();
+function areaSquare(sideSquare){
+     let area = sideSquare * sideSquare;
+     return console.log("El area del cuadrado es: " + area + " cm²");
+}
 
 // triangle
-const sideOne = 6;
-const sideTwo = 6;
-const sideThree = 4;
-const perimeterTriangle = sideOne + sideTwo + sideThree;
-const semiPerimeter = perimeterTriangle/2;
-const areaTriangle = Math.sqrt((semiPerimeter)*(semiPerimeter-sideOne)*(semiPerimeter-sideTwo)*((semiPerimeter-sideThree))); // formula de heron para el area de un triangulo conociendo todos sus lados
+function perimeterTriangle(sideOne, sideTwo, sideThree){
+    let perimeter = sideOne + sideTwo + sideThree;
+    return console.log(`El perimetro del triangulo es: ${perimeter} cm`);
+}
 
-console.group("Triangle's");
-console.log(`Los lados del triangulo miden: ${sideOne}, ${sideTwo} y ${sideThree} cm`);
-console.log(`El perimetro del triangulo es: ${perimeterTriangle} cm`);
-console.log(`El area del triangulo es: ${areaTriangle} cm²`);
-console.groupEnd();
+function areaTriangle(sideOne, sideTwo, sideThree){
+    let semiPerimeter = (sideOne + sideTwo + sideThree)/2;
+    let areaTriangle = Math.sqrt((semiPerimeter)*(semiPerimeter-sideOne)*(semiPerimeter-sideTwo)*((semiPerimeter-sideThree))); // formula de heron para el area de un triangulo conociendo todos sus lados
+    return console.log(`El area del triangulo es: ${Math.round(areaTriangle)} cm²`); 
+}
 
 // circle
+function perimeterCircle(radio){
+    let diameter = radio * 2;
+    let perimeterCircle = Math.PI * diameter;
+    return console.log(`El perimetro del circulo es: ${Math.round(perimeterCircle)} cm`);
+}
 
-const radio = 4;
-const diameter = radio * 2;
-const perimeterCircle = Math.PI * diameter;
-const areaCircle = Math.PI * radio**2;
-
-console.group("Circle");
-console.log(`El perimetro del circulo es: ${perimeterCircle} cm`);
-console.log(`El area del circulo es: ${areaCircle} cm²`);
-console.groupEnd();
+function areaCircle(radio){
+    let areaCircle = Math.PI * radio**2;
+    return console.log(`El area del circulo es: ${Math.round(areaCircle)} cm²`);
+}
