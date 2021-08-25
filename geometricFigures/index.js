1193c766ea4c593eb38e62182209a106ae1d070c
fschambacht <aschambacht@gmail.com>
2021-08-25 14:29:06 -0500

    change language of output results

--- geometricFigures/index.js
+++ geometricFigures/index.js
@@ -2,13 +2,13 @@
 function perimeterSquare(){
     let sideSquare = document.getElementById("sideSquare").valueAsNumber;
     let perimeter = sideSquare * 4;
-    return document.getElementById("result-square").innerHTML= `El perimetro del cuadrado es: ${perimeter} cm`;
+    return document.getElementById("result-square").innerHTML= `The perimeter of square is: ${perimeter} cm`;
 }
 
 function areaSquare(){
     let sideSquare = document.getElementById("sideSquare").valueAsNumber;
     let area = sideSquare * sideSquare;
-    return document.getElementById("result-square").innerHTML= `El area del cuadrado es: ${area} cm²`;
+    return document.getElementById("result-square").innerHTML= `The area of square is: ${area} cm²`;
 }
 
 // triangle
@@ -17,7 +17,7 @@ function perimeterTriangle(){
     let sideTwo = document.getElementById("sideTwo").valueAsNumber;
     let sideThree = document.getElementById("sideThree").valueAsNumber;
     let perimeter = sideOne + sideTwo + sideThree;
-    return document.getElementById("result-triangle").innerHTML= `El perimetro del triangulo es: ${perimeter} cm`;
+    return document.getElementById("result-triangle").innerHTML= `The perimeter of the triangle is: ${perimeter} cm`;
 }
 
 function areaTriangle(){
@@ -26,7 +26,7 @@ function areaTriangle(){
     let sideThree = document.getElementById("sideThree").valueAsNumber;
     let semiPerimeter = (sideOne + sideTwo + sideThree)/2;
     let areaTriangle = Math.round(Math.sqrt((semiPerimeter)*(semiPerimeter-sideOne)*(semiPerimeter-sideTwo)*((semiPerimeter-sideThree)))); // formula de heron para el area de un triangulo conociendo todos sus lados
-    return document.getElementById("result-triangle").innerHTML= `El area del triangulo es: ${areaTriangle} cm²`;
+    return document.getElementById("result-triangle").innerHTML= `The area of triangle is: ${areaTriangle} cm²`;
 }
 
 // circle
@@ -34,11 +34,11 @@ function perimeterCircle(){
     let radio = document.getElementById("radioCircle").valueAsNumber;
     let diameter = radio * 2;
     let perimeterCircle = Math.round(Math.PI * diameter);
-    return document.getElementById("result-circle").innerHTML= `El perimetro del circulo es: ${perimeterCircle} cm`;
+    return document.getElementById("result-circle").innerHTML= `The perimeter of circle is: ${perimeterCircle} cm`;
 }
 
 function areaCircle(){
     let radio = document.getElementById("radioCircle").valueAsNumber;
     let areaCircle = Math.round(Math.PI * radio**2);
-    return document.getElementById("result-circle").innerHTML= `El area del circulo es: ${areaCircle} cm²`;
+    return document.getElementById("result-circle").innerHTML= `The area of circle is: ${areaCircle} cm²`;
 }
