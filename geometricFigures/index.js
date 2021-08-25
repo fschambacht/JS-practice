18df32c691b9e72373adef30b25ed5e16ecb4a68
fschambacht <aschambacht@gmail.com>
2021-08-25 14:24:50 -0500

    add result in html and styles

--- geometricFigures/index.js
+++ geometricFigures/index.js
@@ -2,13 +2,13 @@
 function perimeterSquare(){
     let sideSquare = document.getElementById("sideSquare").valueAsNumber;
     let perimeter = sideSquare * 4;
-    return alert("El perimetro del cuadrado es: " + perimeter + " cm");
+    return document.getElementById("result-square").innerHTML= `El perimetro del cuadrado es: ${perimeter} cm`;
 }
 
 function areaSquare(){
     let sideSquare = document.getElementById("sideSquare").valueAsNumber;
     let area = sideSquare * sideSquare;
-    return alert("El area del cuadrado es: " + area + " cm²");
+    return document.getElementById("result-square").innerHTML= `El area del cuadrado es: ${area} cm²`;
 }
 
 // triangle
@@ -17,7 +17,7 @@ function perimeterTriangle(){
     let sideTwo = document.getElementById("sideTwo").valueAsNumber;
     let sideThree = document.getElementById("sideThree").valueAsNumber;
     let perimeter = sideOne + sideTwo + sideThree;
-    return alert("El perimetro del triangulo es: " + perimeter + " cm");
+    return document.getElementById("result-triangle").innerHTML= `El perimetro del triangulo es: ${perimeter} cm`;
 }
 
 function areaTriangle(){
@@ -25,20 +25,20 @@ function areaTriangle(){
     let sideTwo = document.getElementById("sideTwo").valueAsNumber;
     let sideThree = document.getElementById("sideThree").valueAsNumber;
     let semiPerimeter = (sideOne + sideTwo + sideThree)/2;
-    let areaTriangle = Math.sqrt((semiPerimeter)*(semiPerimeter-sideOne)*(semiPerimeter-sideTwo)*((semiPerimeter-sideThree))); // formula de heron para el area de un triangulo conociendo todos sus lados
-    return alert(`El area del triangulo es: ${Math.round(areaTriangle)} cm²`); 
+    let areaTriangle = Math.round(Math.sqrt((semiPerimeter)*(semiPerimeter-sideOne)*(semiPerimeter-sideTwo)*((semiPerimeter-sideThree)))); // formula de heron para el area de un triangulo conociendo todos sus lados
+    return document.getElementById("result-triangle").innerHTML= `El area del triangulo es: ${areaTriangle} cm²`;
 }
 
 // circle
 function perimeterCircle(){
     let radio = document.getElementById("radioCircle").valueAsNumber;
     let diameter = radio * 2;
-    let perimeterCircle = Math.PI * diameter;
-    return alert(`El perimetro del circulo es: ${Math.round(perimeterCircle)} cm`);
+    let perimeterCircle = Math.round(Math.PI * diameter);
+    return document.getElementById("result-circle").innerHTML= `El perimetro del circulo es: ${perimeterCircle} cm`;
 }
 
 function areaCircle(){
     let radio = document.getElementById("radioCircle").valueAsNumber;
-    let areaCircle = Math.PI * radio**2;
-    return alert(`El area del circulo es: ${Math.round(areaCircle)} cm²`);
+    let areaCircle = Math.round(Math.PI * radio**2);
+    return document.getElementById("result-circle").innerHTML= `El area del circulo es: ${areaCircle} cm²`;
 }
